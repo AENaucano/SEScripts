1320d063f4b0d2f2debf2a4efd57403a7090b65f
AENaucano <43579461+AENaucano@users.noreply.github.com>
2026-04-27 15:44:52 +0200

    for VSC 1.13

--- BIOS.cs
+++ BIOS.cs
@@ -1,4 +1,3 @@
-        public string ScriptTag = "VSC"; // name of this script
         public string ShipTag = "VSCShip"; // name of the ship (grid) -> Program
         public string PAMTag = "PAM"; // to be set to correct Name
         public string SpecialTag = "IIM"; // in accordance with Isy's inventory manager system 
@@ -7,34 +6,37 @@
         public string SorterIn = "SorterIn"; // Name the sorter for loading
         public string SorterOut = "SorterOut"; // Name the sorter for delivery
 
+        public bool DoScan()
+        {        
+            if (String.IsNullOrWhiteSpace(Me.CustomData)) { SetDefCustomData(); }
+            else {ScanCustomData(); }
+            ...
+        }
+
         public void ScanCustomData()
         {
-            if (String.IsNullOrWhiteSpace(Me.CustomData)) { SetDefCustomData(); return; }
-            ShipTag = BaSe.GetCustomDataTag(Me, "ShipTag");
+            ShipTag = BaSe.GetCustomDataTag(Me, "ShipTag",  "VSCShip" );
             if(ShipTag != Me.CubeGrid.CustomName) { Message += "ShipTag does not match grid name\n"; SetDefCustomData(); return; }
-            PAMTag = BaSe.GetCustomDataTag(Me, "PAMTag");
-            SpecialTag = BaSe.GetCustomDataTag(Me, "SpecialTag"); 
-            HomeTag = BaSe.GetCustomDataTag(Me, "HomeTag");
-            DepotTag = BaSe.GetCustomDataTag(Me, "DepotTag");
-            SorterIn = BaSe.GetCustomDataTag(Me, "SorterIn");
-            SorterOut = BaSe.GetCustomDataTag(Me, "SorterOut");
+            PAMTag = BaSe.GetCustomDataTag(Me, "PAMTag", "PAM");
+            SpecialTag = BaSe.GetCustomDataTag(Me, "SpecialTag","IIM"); 
+            HomeTag = BaSe.GetCustomDataTag(Me, "HomeTag", "HQ");
+            DepotTag = BaSe.GetCustomDataTag(Me, "DepotTag", "Depot");
+            SorterIn = BaSe.GetCustomDataTag(Me, "SorterIn", "SorterIn");
+            SorterOut = BaSe.GetCustomDataTag(Me, "SorterOut", "SorterOut");
             // parameters
-            string IntIn = BaSe.GetCustomDataTag(Me, "MaxTime");
+            string IntIn = BaSe.GetCustomDataTag(Me, "MaxTime", "1");
             Int32.TryParse(IntIn, out MaxTime);
-            IntIn = BaSe.GetCustomDataTag(Me, "WaitForCargo");
+            IntIn = BaSe.GetCustomDataTag(Me, "WaitForCargo", "100");
             Int32.TryParse(IntIn, out WaitForCargo);
-            IntIn = BaSe.GetCustomDataTag(Me, "ConsolePrn");
+            IntIn = BaSe.GetCustomDataTag(Me, "ConsolePrn","-1");
             int prnConsole = -1;
             Int32.TryParse(IntIn, out prnConsole);
             Screen.ConsolePrn = prnConsole;
-            // IMyCockpit
-            string CockpitName = BaSe.GetCustomDataTag(Me, "Console");
-            Screen.Console = Grid.GetBlockWithName(CockpitName) as IMyCockpit;
             // screen
-            string _font = BaSe.GetCustomDataTag(Me, "Font");
+            string _font = BaSe.GetCustomDataTag(Me, "Font","Monospace");
             Screen.myFont = _font;
             float InFloat = 1.0f;
-            if (!Single.TryParse(BaSe.GetCustomDataTag(Me, "SizeFont"), out InFloat)) InFloat = 1.0f;
+            if (!Single.TryParse(BaSe.GetCustomDataTag(Me, "SizeFont","1.0"), out InFloat)) InFloat = 1.0f;
             Screen.myFontSize = InFloat;
 
             return;
@@ -44,19 +46,17 @@
             string newData = "";
             Me.CustomData = "";
             newData += "//ShipTag=" + Me.CubeGrid.CustomName + "\n";
-            newData += "//PAMTag=" + PAMTag + "\n";
-            newData += "//SpecialTag=" + SpecialTag + "\n";
-            newData += "//HomeTag=" + HomeTag + "\n";
-            newData += "//DepotTag=" + DepotTag + "\n";
-            newData += "//SorterIn=" + SorterIn + "\n";
-            newData += "//SorterOut=" + SorterOut + "\n";
+            newData += "//PAMTag=VSCShip\n";
+            newData += "//SpecialTag= IIM\n";
+            newData += "//HomeTag=HQ\n";
+            newData += "//DepotTag=Depot\n";
+            newData += "//SorterIn=SorterIn\n";
+            newData += "//SorterOut=SorterOut\n";
             // parameters
-            newData += "//MaxTime=" + MaxTime + "\n";
-            newData += "//WaitForCargo=" + WaitForCargo.ToString() + "\n";
-            newData += "//ConsolePrn=" + Screen.ConsolePrn.ToString() + "\n";
+            newData += "//MaxTime=1\n";
+            newData += "//WaitForCargo=100\n";
+            newData += "//ConsolePrn=0\n";
             // cockpit
-            if (Screen.Console != null) newData += "//Console=" + Screen.Console.CustomName + "\n";
-            else newData += "//Console=" + ShipTag + " Main Cockpit\n";
             newData += "//Font = Monospace\n";
             newData += "//FontSize = 1.0\n";
 
@@ -64,33 +64,42 @@
             return;
         }
 
+        public string EchoChars = "//"; // space gives problems
 
-public string GetCustomDataTag(IMyTerminalBlock thisBlock, string _thisTag, string _setThis = "")
-          {
-                if (String.IsNullOrWhiteSpace(thisBlock.CustomData)) return "";
-                string _CustomData = thisBlock.CustomData.Trim();
-                string _newCD = "";
-                bool found = false;
+        public string GetCustomDataTag(IMyTerminalBlock thisBlock, string _thisTag, string _Default)
+        {
+            string _CustomData = thisBlock.CustomData.Trim();
+            string _newCD = "";
+            string found = "";
 
-                string[] _cdlines = _CustomData.Split('\n');
-                // for each line
-                for (int cdidx = 0; cdidx < _cdlines.Length; cdidx++)
+            string[] _cdlines = _CustomData.Split('\n');
+            
+            for (int cdidx = 0; cdidx < _cdlines.Length; cdidx++)
+            {
+                // if it does not start with // it is not mine !
+                if (!_cdlines[cdidx].StartsWith(EchoChars))
                 {
-                    // if it does not start with // it is not mine !
-                    if (_cdlines[cdidx].StartsWith(EchoChars))
+                    _newCD += _cdlines[cdidx] + "\n";
+                }
+                else
+                {
+                    string _cdline = _cdlines[cdidx].Replace(EchoChars, "");
+                    string[] _cdwords = _cdline.Split('=');
+                    if (_cdwords[0].Trim().ToLower() == _thisTag.Trim().ToLower())
                     {
-                        string _cdline = _cdlines[cdidx].Replace(EchoChars, "");
-                        string[] _cdwords = _cdline.Split('=');
-                        if (_cdwords[0].Trim().ToLower() == _thisTag.Trim().ToLower())
-                        {
-                            found = true;
-                            // replace
-                            if (_setThis != "") { _newCD += EchoChars + _thisTag + "=" + _setThis + "\n"; }
-                            else { return _cdwords[1]; } // just return current data
-                        }
+                        if ( _cdwords[1].Trim() == "") { found=_Default;}
+                        else { found = _cdwords[1]; }
+                        _newCD += EchoChars + _thisTag + "=" + found + "\n";
                     }
                     else
                     {
-                        _newCD += _cdlines[cdidx] + "\n";
+                        _newCD += EchoChars + _cdwords[0] + "=" + _cdwords[1] + "\n";
                     }
                 }
+            }
+            // insert new entry
+            if(found == "" ) { _newCD += EchoChars + _thisTag + "=" + _Default + "\n"; found=_Default;}
+            Me.CustomData = _newCD;
+
+            return found;
+        }
