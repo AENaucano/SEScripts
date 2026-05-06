0fd1f47b02dd2f8a2d80e3d33b0061f8821a9601
AENaucano <43579461+AENaucano@users.noreply.github.com>
2026-05-06 15:34:20 +0200

    made snippet

--- Thruster.cs
+++ Thruster.cs
@@ -12,7 +12,48 @@ IMyThrust
 public float CurrentThrust { get; }
 public float MaxEffectiveThrust { get; }
 
+MaxEffectiveThrust: This is the maximum possible force (in Newtons) the thruster can generate in its current environment. 
+Unlike MaxThrust (the raw design limit), this value accounts for environmental penalties, such as an Ion Thruster's reduced efficiency in an atmosphere or an Atmospheric Thruster's loss of power at high altitudes.
+
+CurrentThrust: This is the actual force (in Newtons) the thruster is producing right now.
+It changes dynamically based on player input (WASD), inertial dampener corrections, or a specific ThrustOverride setting.
+
+F>m*g
+Where:
+F: Total MaxEffectiveThrust of all downward-facing thrusters (in Newtons).
+m: Total grid mass (in kilograms), found in the Info tab of your terminal.
+g: Local gravity (in m/s^2).
+
+On an Earth-like planet, this is approximately 9.81 m/s^2\ (often rounded to \(10\) for a safety buffer)
+
 IMyCockpit
 
 GetTotalGravity
 CalculateShipMass
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+List<IMyThrust> UpThrusters = new list<IMyThrust>();
+
+list<IMyThrust> Thrusters =  new list<IMyThrust>();
+Grid.GetBlocksOfType(Thrusters, ThatsMe_Grid);
+if (!Thrusters.Any()) { Message += "Thrusters missing\n"; return false; }
+for (int tidx = 0; tidx < Thrusters.Count(); tidx++)
+{
+    Vector3I direction = Thruster[tidx].GridThrustDirection;
+    if (direction == Vector3I.Up) UpThrusters.Add(Thruster[tidx]);
+}
+
+public float AllThrust=0;
+Public float EffThrust=0;
+public float Checkthrust()
+{
+    AllThrust=0;
+    EffThrust=0;
+    for (int tidx = 0; tidx < UpThrusters.Count(); tidx++)
+    {
+        AllThrust += UpThrusters.CurrentThrust;
+        EffThrust += UpThrusters.MaxEffectiveThrust;
+    }
+    return AllThrust;
+}
+
